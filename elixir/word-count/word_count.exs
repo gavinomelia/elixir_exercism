4d52dc0b153af6ea01954aee5eb608fdb8143608
Gavin OMelia <gavin@omelia.org>
2016-01-22 06:28:07 -0500

    Second iteration of word count

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -1,13 +1,15 @@
 defmodule Words do
-  @doc """
-  Count the number of words in the sentence.
-
-  Words are compared case-insensitively.
-  """
-
   def count(sentence) do
-    ignore_punctuation_and_case = sentence |> String.downcase |> String.replace(~r([^[:alpha:]|\d-])u, " ")
-    split = String.split(ignore_punctuation_and_case)
+    ignore_punctuation_and_case(sentence) |>
+    String.split |>
+    count_map
+  end
+
+  defp count_map(split) do
     Map.new(split, fn(x) -> {x, Enum.count(split, fn(c) -> c == x end)} end)
+  end
+
+  defp ignore_punctuation_and_case(sentence) do
+    sentence |> String.downcase |> String.replace(~r([^[:alpha:]|\d-])u, " ")
   end
 end
