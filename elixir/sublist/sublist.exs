17ecb7c1d85e311233efda426f4cbe7be2523999
Gavin OMelia <gavin@omelia.org>
2016-03-07 08:56:14 -0500

    Finish up Sublist

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -1,24 +1,24 @@
 defmodule Sublist do
-  def compare(a, a), do: :equal
-  def compare(a, b) do
-    if Enum.count(a) < Enum.count(b) do
-    cond do
-      is_contained?(a, b) ->
-        :sublist
-      true ->
-        :unequal
-      end
-    else
-      cond do
-        is_contained?(b, a) ->
-          :superlist
-        true ->
-          :unequal
-        end
-      end
-    end
 
-  def is_contained?(a, b) do
-    Enum.all?(a, fn(x) -> Enum.member?(b, x) end)
+  def compare(a, a), do: :equal
+
+  def compare(a, b) do
+    cond do
+      sublist?(a, b, a, b) -> :sublist
+      sublist?(b, a, b, a) -> :superlist
+      true -> :unequal
+    end
   end
+
+  defp sublist?([], _, _, _), do: true
+  defp sublist?(_, [], _, _), do: false
+
+  defp sublist?([head | a_tail], [head | b_tail], original_a, original_b) do
+    sublist?(a_tail, b_tail, original_a, original_b)
+  end
+
+  defp sublist?([_|_], [_|_], original_a, [_|orig_b_tail]) do
+    sublist?(original_a, orig_b_tail, original_a, orig_b_tail)
+  end
+
 end
--- elixir/sublist/sublist_test.exs
+++ elixir/sublist/sublist_test.exs
@@ -51,7 +51,6 @@ defmodule SublistTest do
     assert Sublist.compare([3,4,5], Enum.to_list(1..1_000_000)) == :sublist
   end
 
-  @tag :pending
   test "huge sublist not in huge list" do
     assert Sublist.compare(Enum.to_list(10..1_000_001),
     Enum.to_list(1..1_000_000))
@@ -90,7 +89,6 @@ defmodule SublistTest do
     assert Sublist.compare([1,2,1,2,3], [1,2,3,1,2,1,2,3,2,1]) == :sublist
   end
 
-  # @tag :pending
   test "recurring values unequal" do
     assert Sublist.compare([1,2,1,2,3], [1,2,3,1,2,3,2,3,2,1]) == :unequal
   end
