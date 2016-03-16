7c08d3692f454f935d8d8c28cb5502da7ebaaf76
Gavin OMelia <gavin@omelia.org>
2016-03-16 07:51:36 -0400

    Use Enum.filter in another iteration of Anagram

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -1,31 +1,14 @@
 defmodule Anagram do
   def match(base, words) do
-    anagram(base, words)
+    Enum.filter(words, fn(x) ->
+      is_anagram?(base |> String.downcase, x |> String.downcase)
+    end)
   end
-
-  def anagram(base, words) do
-    do_anagram(base, words, [])
-  end
-
-  defp do_anagram(b, [w|other_words], acc) do
-    base = String.downcase(b)
-    word = String.downcase(w)
-    if is_anagram?(base, word) do
-      do_anagram(b, other_words, [w|acc])
-    else
-      do_anagram(b, other_words, acc)
-    end
-  end
-  defp do_anagram(_, [], acc), do: acc |> Enum.reverse
 
   defp is_anagram?(word, word), do: false
   defp is_anagram?(base, word) do
-    each_letter_matches?(base, word)
+    split_and_sort_it_up(base) == split_and_sort_it_up(word)
   end
 
   defp split_and_sort_it_up(string), do: String.codepoints(string) |> Enum.sort
-
-  defp each_letter_matches?(base, word) do
-    split_and_sort_it_up(base) == split_and_sort_it_up(word)
-  end
 end
