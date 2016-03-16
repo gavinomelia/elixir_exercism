69c78011beafc4c3772f708ec2d6ac3ed647e5d7
Gavin OMelia <gavin@omelia.org>
2016-03-16 07:07:36 -0400

    Submit another Anagram iteration

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -10,16 +10,18 @@ defmodule Anagram do
   defp do_anagram(b, [w|other_words], acc) do
     base = String.downcase(b)
     word = String.downcase(w)
-    cond do
-      base == word ->
-        do_anagram(b, other_words, acc)
-      each_letter_matches?(base, word) ->
-        do_anagram(b, other_words, [w|acc])
-      true ->
-        do_anagram(b, other_words, acc)
+    if is_anagram?(base, word) do
+      do_anagram(b, other_words, [w|acc])
+    else
+      do_anagram(b, other_words, acc)
     end
   end
   defp do_anagram(_, [], acc), do: acc |> Enum.reverse
+
+  defp is_anagram?(word, word), do: false
+  defp is_anagram?(base, word) do
+    each_letter_matches?(base, word)
+  end
 
   defp split_and_sort_it_up(string), do: String.codepoints(string) |> Enum.sort
 
