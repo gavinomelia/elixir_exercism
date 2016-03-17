a12f7fdda6af23f3f906c8c8f62ec45d6852797e
Gavin OMelia <gavin@omelia.org>
2016-03-17 07:18:51 -0400

    RNA Transcription

--- elixir/rna-transcription/dna.exs
+++ elixir/rna-transcription/dna.exs
@@ -1,14 +1,6 @@
 defmodule DNA do
-  @doc """
-  Transcribes a character list representing DNA nucleotides to RNA
-
-  ## Examples
-
-  iex> DNA.to_rna('ACTG')
-  'UGAC'
-  """
-  @spec to_rna([char]) :: [char]
   def to_rna(dna) do
-
+    dna_rna_map = %{?G => 'C', ?C => 'G', ?T => 'A', ?A => 'U'}
+    Enum.flat_map(dna, fn(x) -> Map.fetch!(dna_rna_map, x) end)
   end
 end
