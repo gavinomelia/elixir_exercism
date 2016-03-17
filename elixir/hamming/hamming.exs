dcbbb64f3e24d3c8c1f56a3cec3ee9a2642eabc7
Gavin OMelia <gavin@omelia.org>
2016-03-17 07:56:56 -0400

    Do a lot of work on Hamming

--- elixir/hamming/hamming.exs
+++ elixir/hamming/hamming.exs
@@ -1,4 +1,14 @@
 defmodule DNA do
-  def hamming_distance(strand1, strand2) do
+  def hamming_distance(strand1, strand2) when length(strand1) == length(strand2) do
+    do_hamming_distance(strand1, strand2, 0)
   end
+  def hamming_distance(_, _), do: nil
+
+  defp do_hamming_distance([head|a_tail], [head|b_tail], acc) do
+    do_hamming_distance(a_tail, b_tail, acc)
+  end
+  defp do_hamming_distance([_|a_tail], [_|b_tail], acc) do
+    do_hamming_distance(a_tail, b_tail, acc + 1)
+  end
+  defp do_hamming_distance([], [], acc), do: acc
 end
