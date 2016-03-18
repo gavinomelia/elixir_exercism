08e9b19c8e6ef2d6b6402f87ccb9765206b97843
Gavin OMelia <gavin@omelia.org>
2016-03-18 07:22:49 -0400

    Second iteration of Hamming

--- elixir/hamming/hamming.exs
+++ elixir/hamming/hamming.exs
@@ -1,14 +1,7 @@
 defmodule DNA do
   def hamming_distance(strand1, strand2) when length(strand1) == length(strand2) do
-    do_hamming_distance(strand1, strand2, 0)
+    zipped_strands = Enum.zip(strand1, strand2)
+    Enum.count(zipped_strands, fn({x, y}) -> x != y end)
   end
   def hamming_distance(_, _), do: nil
-
-  defp do_hamming_distance([head|a_tail], [head|b_tail], acc) do
-    do_hamming_distance(a_tail, b_tail, acc)
-  end
-  defp do_hamming_distance([_|a_tail], [_|b_tail], acc) do
-    do_hamming_distance(a_tail, b_tail, acc + 1)
-  end
-  defp do_hamming_distance([], [], acc), do: acc
 end
