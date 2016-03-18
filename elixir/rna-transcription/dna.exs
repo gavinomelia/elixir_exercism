f91644f5085a1f99891d1b43044e5bef7d1ce226
Gavin OMelia <gavin@omelia.org>
2016-03-18 07:01:17 -0400

    Second iteration of RNA Transcription

--- elixir/rna-transcription/dna.exs
+++ elixir/rna-transcription/dna.exs
@@ -1,6 +1,6 @@
 defmodule DNA do
   def to_rna(dna) do
-    dna_rna_map = %{?G => 'C', ?C => 'G', ?T => 'A', ?A => 'U'}
-    Enum.flat_map(dna, fn(x) -> Map.fetch!(dna_rna_map, x) end)
+    dna_rna_map = %{?G => ?C, ?C => ?G, ?T => ?A, ?A => ?U}
+    Enum.map(dna, fn(x) -> dna_rna_map[x] end)
   end
 end
