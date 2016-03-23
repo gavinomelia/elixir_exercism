de078af8bfb96ed45b83b36db8b08135918d3165
Gavin OMelia <gavin@omelia.org>
2016-03-23 07:25:24 -0400

    Another iteration of Phone Number

--- elixir/phone-number/phone_number.exs
+++ elixir/phone-number/phone_number.exs
@@ -3,21 +3,12 @@ defmodule Phone do
     clean(raw) |> valid_check
   end
 
-  defp valid_check(clean_num) when byte_size(clean_num) > 10 do
-    new_num = check_1(clean_num)
-    if byte_size(new_num) > 10 do
-      "0000000000"
-    else
-      valid_check(new_num)
-    end
+  defp valid_check(clean_num) when byte_size(clean_num) == 10 do
+    clean_num
   end
-  defp valid_check(clean_num) when byte_size(clean_num) < 10 do
+  defp valid_check("1" <> rest), do: valid_check(rest)
+  defp valid_check(clean_num) when byte_size(clean_num) != 10 do
     "0000000000"
-  end
-  defp valid_check(clean_num), do: clean_num
-
-  defp check_1(clean_num) do
-    String.replace_prefix(clean_num, "1", "")
   end
 
   defp clean(raw), do: String.replace(raw, ~r/(\W)/, "")
@@ -26,9 +17,11 @@ defmodule Phone do
     String.slice(number(raw), 0..2)
   end
 
+  def slice_number(raw, range) do
+    String.slice(number(raw), range)
+  end
+
   def pretty(raw) do
-    first_group = String.slice(number(raw), 3..5)
-    second_group = String.slice(number(raw), 6..10)
-    "(#{area_code(raw)}) #{first_group}-#{second_group}"
+    "(#{area_code(raw)}) #{slice_number(raw, 3..5)}-#{slice_number(raw, 6..10)}"
   end
 end
