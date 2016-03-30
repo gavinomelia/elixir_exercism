7a224789281fcc7135bb03462310100f0c79f14f
Gavin OMelia <gavin@omelia.org>
2016-03-30 06:33:00 -0400

    Get Grade School to a working state

--- elixir/grade-school/school.exs
+++ elixir/grade-school/school.exs
@@ -1,24 +1,24 @@
 defmodule School do
 
   def add(db, name, grade) do
-    if !Map.has_key?(db, grade) do
-      Map.put(db, grade, [name])
-    else
+    if Map.has_key?(db, grade) do
       Map.update!(db, grade, fn(x) -> x ++ [name] end)
+    else
+      Map.put(db, grade, [name])
     end
   end
 
   def grade(db, grade) do
-    if !Map.has_key?(db, grade) do
-      []
-    else
+    if Map.has_key?(db, grade) do
       db[grade]
+    else
+      []
     end
   end
 
   def sort(db) do
-    db_list = Map.to_list(db)
-    sorted_list = Enum.sort(db_list)
-    Enum.map(sorted_list, fn({x, y}) -> {x, y |> Enum.sort} end)
+    db
+    |> Map.to_list
+    |> Enum.map(fn({x, y}) -> {x, y |> Enum.sort} end)
   end
 end
