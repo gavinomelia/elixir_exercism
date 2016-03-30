124cbd82aba4a2f3e551515f520367ea23f3cddd
Gavin OMelia <gavin@omelia.org>
2016-03-30 07:18:15 -0400

    Get Leap to a working state

--- elixir/leap/README.md
+++ elixir/leap/README.md
@@ -1,11 +1,4 @@
 # Leap
-Returns whether 'year' is a leap year.
-
-A leap year occurs:
-
-on every year that is evenly divisible by 4
-  except every year that is evenly divisible by 100
-    unless the year is also evenly divisible by 400
 
 Write a program that will take a year and report if it is a leap year.
 
--- elixir/leap/leap.exs
+++ elixir/leap/leap.exs
@@ -1,5 +1,16 @@
 defmodule Year do
-  def leap_year?(year) do
-
+  def leap_year?(year) when rem(year, 4) == 0 do
+    check_100?(year)
   end
+  def leap_year?(_), do: false
+
+  defp check_100?(year) when rem(year, 100) == 0 do
+    check_400?(year)
+  end
+  defp check_100?(_), do: true
+
+  defp check_400?(year) when rem(year, 400) == 0 do
+    true
+  end
+  defp check_400?(_), do: false
 end
