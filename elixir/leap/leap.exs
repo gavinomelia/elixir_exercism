663a29c99608f559962d8621a3f77766668911b6
Gavin OMelia <gavin@omelia.org>
2016-04-07 07:45:50 -0400

    Rename variables in Leap

--- elixir/leap/leap.exs
+++ elixir/leap/leap.exs
@@ -1,16 +1,16 @@
 defmodule Year do
   def leap_year?(year) when rem(year, 4) == 0 do
-    check_100?(year)
+    div_100?(year)
   end
   def leap_year?(_), do: false
 
-  defp check_100?(year) when rem(year, 100) == 0 do
-    check_400?(year)
+  defp div_100?(year) when rem(year, 100) == 0 do
+    div_400?(year)
   end
-  defp check_100?(_), do: true
+  defp div_100?(_), do: true
 
-  defp check_400?(year) when rem(year, 400) == 0 do
+  defp div_400?(year) when rem(year, 400) == 0 do
     true
   end
-  defp check_400?(_), do: false
+  defp div_400?(_), do: false
 end
