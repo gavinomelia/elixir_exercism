f4669353398ad040cf4dd3681b8d4eba970e7f6f
Gavin OMelia <gavin@omelia.org>
2016-04-13 06:39:20 -0400

    Simplify Leap Year

--- elixir/leap/leap.exs
+++ elixir/leap/leap.exs
@@ -1,16 +1,9 @@
 defmodule Year do
-  def leap_year?(year) when rem(year, 4) == 0 do
-    div_100?(year)
-  end
-  def leap_year?(_), do: false
-
-  defp div_100?(year) when rem(year, 100) == 0 do
-    div_400?(year)
-  end
-  defp div_100?(_), do: true
-
-  defp div_400?(year) when rem(year, 400) == 0 do
+  def leap_year?(year) when rem(year, 400) == 0 do
     true
   end
-  defp div_400?(_), do: false
+  def leap_year?(year) when rem(year, 100) == 0 do
+    false
+  end
+  def leap_year?(year), do: rem(year, 4) == 0
 end
