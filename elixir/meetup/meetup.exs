089e022a26c2f7253457250dbef0af5d9b027a91
Gavin OMelia <gavin@omelia.org>
2016-05-02 07:03:23 -0400

    Fetch meetup

---
+++ elixir/meetup/meetup.exs
@@ -0,0 +1,15 @@
+defmodule Meetup do
+  @moduledoc """
+  Calculate meetup dates.
+  """
+
+  @type weekday ::
+      :monday | :tuesday | :wednesday
+    | :thursday | :friday | :saturday | :sunday
+
+  @type schedule :: :first | :second | :third | :fourth | :last | :teenth
+
+  def meetup(year, month, weekday, schedule) do
+
+  end
+end
