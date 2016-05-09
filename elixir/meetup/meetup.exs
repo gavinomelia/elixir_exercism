e2aa18209f3ad474c182d27c13c650699fe51db2
Gavin OMelia <gavin@omelia.org>
2016-05-09 08:58:10 -0400

    Make a few tweaks to meetup

--- elixir/meetup/meetup.exs
+++ elixir/meetup/meetup.exs
@@ -1,15 +1,16 @@
 defmodule Meetup do
-  @week %{1 => :monday, 2 => :tuesday, 3 => :wednesday, 4 => :thursday, 5 => :friday, 6 => :saturday, 7 => :sunday}
   def meetup(year, month, weekday, mode) do
     num_of_days = :calendar.last_day_of_the_month(year, month)
-    corr_thing = %{:first => 1, :second => 8, :third => 15,
+    corr_num = %{:first => 1, :second => 8, :third => 15,
     :fourth => 22, :last => num_of_days, :teenth => 13}
-    get_day(year, month, weekday, corr_thing[mode], mode)
+    get_day(year, month, weekday, corr_num[mode], mode)
   end
 
   def get_day(year, month, weekday, acc, mode) do
+    week = %{1 => :monday, 2 => :tuesday, 3 => :wednesday, 4 => :thursday,
+    5 => :friday, 6 => :saturday, 7 => :sunday}
     day = :calendar.day_of_the_week(year, month, acc)
-    if @week[day] == weekday do
+    if week[day] == weekday do
       {year, month, acc}
     else
       get_day(year, month, weekday, operator(acc, mode), mode)
