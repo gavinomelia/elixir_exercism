c960a8895a87bd82241cc079e7ae4e77ce99d94e
Gavin OMelia <gavin@omelia.org>
2016-05-27 07:59:03 -0400

    Get tests working for Change

--- elixir/change/change.exs
+++ elixir/change/change.exs
@@ -1,21 +1,26 @@
 defmodule Change do
-  @doc """
-    Determine the least number of coins to be given to the user such
-    that the sum of the coins' value would equal the correct amount of change.
-    It returns :error if it is not possible to compute the right amount of coins.
-    Otherwise returns the tuple {:ok, map_of_coins}
-
-    ## Examples
-
-      iex> Change.generate(3, [5, 10, 15])
-      :error
-
-      iex> Change.generate(18, [1, 5, 10])
-      {:ok, %{1 => 3, 5 => 1, 10 => 1}}
-
-  """
-
+  def generate(_, []), do: :error
   def generate(amount, values) do
+    change_map = get_change(amount, values)
+    if no_change?(change_map) == true do
+      :error
+    else
+      {:ok, change_map}
+    end
+  end
 
+  def get_change(amount, values) do
+    do_get_change(amount, values |> Enum.sort(&(&1 > &2)), %{})
+  end
+  def do_get_change(amount, [], acc2), do: acc2
+  def do_get_change(amount, [head|tail], acc2) do
+    divisor = div(amount, head)
+    do_get_change(amount - divisor * head, tail, Map.put(acc2, head, divisor))
+  end
+
+  def no_change?(change_map) do
+    change_map
+    |> Map.values
+    |> Enum.all?(fn(x) -> x == 0 end)
   end
 end
