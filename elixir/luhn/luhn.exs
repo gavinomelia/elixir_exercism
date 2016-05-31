1167dc7440201b660c7bfcabb012a8a74872357b
Gavin OMelia <gavin@omelia.org>
2016-05-31 07:17:01 -0400

    Fetch Luhn

---
+++ elixir/luhn/luhn.exs
@@ -0,0 +1,20 @@
+defmodule Luhn do
+  @doc """
+  Calculates the total checksum of a number
+  """
+  def checksum(number) do
+  end
+
+  @doc """
+  Checks if the given number is valid via the luhn formula
+  """
+  def valid?(number) do
+  end
+
+  @doc """
+  Creates a valid number by adding the correct
+  checksum digit to the end of the number
+  """
+  def create(number) do
+  end
+end
