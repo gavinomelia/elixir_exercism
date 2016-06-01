fbad804cb6bf87b52216c74927a523cee1564b72
Gavin OMelia <gavin@omelia.org>
2016-06-01 08:20:33 -0400

    Improve variable name in Gigasecond

--- elixir/gigasecond/gigasecond.exs
+++ elixir/gigasecond/gigasecond.exs
@@ -1,7 +1,7 @@
 defmodule Gigasecond do
 	def from(date_time) do
-		current_seconds = :calendar.datetime_to_gregorian_seconds(date_time)
-		total_seconds = current_seconds + 1000000000
+		seconds_in_date = :calendar.datetime_to_gregorian_seconds(date_time)
+		total_seconds = seconds_in_date + 1000000000
 		:calendar.gregorian_seconds_to_datetime(total_seconds)
 	end
 end
