2d94116ba1570c3de2b221646aa6b0f8c7ccf677
Gavin OMelia <gavin@omelia.org>
2016-06-01 08:15:33 -0400

    Get Gigasecond working

--- elixir/gigasecond/gigasecond.exs
+++ elixir/gigasecond/gigasecond.exs
@@ -1,5 +1,7 @@
 defmodule Gigasecond do
-	def from({{year, month, day}, {hours, minutes, seconds}}) do
-
+	def from(date_time) do
+		current_seconds = :calendar.datetime_to_gregorian_seconds(date_time)
+		total_seconds = current_seconds + 1000000000
+		:calendar.gregorian_seconds_to_datetime(total_seconds)
 	end
 end
