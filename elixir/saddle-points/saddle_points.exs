395446cb310bbc01b483f53bf3d16b661f847265
Gavin OMelia <gavin@omelia.org>
2016-07-11 10:58:29 -0400

    Fetch Saddle Points

---
+++ elixir/saddle-points/saddle_points.exs
@@ -0,0 +1,25 @@
+defmodule Matrix do
+  @doc """
+  Parses a string representation of a matrix
+  to a list of rows
+  """
+  def rows(str) do
+
+  end
+
+  @doc """
+  Parses a string representation of a matrix
+  to a list of columns
+  """
+  def columns(str) do
+
+  end
+
+  @doc """
+  Calculates all the saddle points from a string
+  representation of a matrix
+  """
+  def saddle_points(str) do
+
+  end
+end
