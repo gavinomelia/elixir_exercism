f78fdc7aa0baf91659fe9b444856f837ab7f4b79
Gavin OMelia <gavin@omelia.org>
2016-07-21 15:14:43 -0400

    Fetch Triangle

---
+++ elixir/triangle/triangle.exs
@@ -0,0 +1,10 @@
+defmodule Triangle do
+  @type kind :: :equilateral | :isosceles | :scalene
+
+  @doc """
+  Return the kind of triangle of a triangle with 'a', 'b' and 'c' as lengths.
+  """
+  def kind(a, b, c) do
+
+  end
+end
