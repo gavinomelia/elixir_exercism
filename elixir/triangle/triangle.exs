afa563f48bb16921003b06341506f984b750e2e1
Gavin OMelia <gavin@omelia.org>
2016-07-25 10:37:12 -0400

    Refactor Triangle some

--- elixir/triangle/triangle.exs
+++ elixir/triangle/triangle.exs
@@ -1,9 +1,7 @@
 defmodule Triangle do
-  @doc """
-  Return the kind of triangle of a triangle with 'a', 'b' and 'c' as lengths.
-  """
   def is_valid([a, b, c], kind) do
     cond do
+      Enum.any?([a, b, c], fn(x) -> x <= 0 end) -> {:error, "all side lengths must be positive"}
       a + b <= c -> {:error, "side lengths violate triangle inequality"}
       a + c <= b -> {:error, "side lengths violate triangle inequality"}
       b + c <= a -> {:error, "side lengths violate triangle inequality"}
@@ -11,11 +9,7 @@ defmodule Triangle do
     end
   end
 
-  def kind(a, _, _) when a <= 0, do: {:error, "all side lengths must be positive"}
-  def kind(_, b, _) when b <= 0, do: {:error, "all side lengths must be positive"}
-  def kind(_, _, c) when c <= 0, do: {:error, "all side lengths must be positive"}
-
-  def kind(a, a, a), do: {:ok, :equilateral}
+  def kind(a, a, a), do: is_valid([a, a, a], :equilateral)
 
   def kind(a, a, b), do: is_valid([a, a, b], :isosceles)
   def kind(a, b, a), do: is_valid([a, b, a], :isosceles)
