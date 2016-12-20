96c550ce76938b6adfd14251755ddc0ab331a598
Gavin OMelia <gavin@omelia.org>
2016-12-20 12:15:03 -0500

    roman numeral stuff

--- elixir/roman-numerals/roman.exs
+++ elixir/roman-numerals/roman.exs
@@ -1,26 +1,66 @@
 defmodule Roman do
-  @numeral_map %{1000 => "M", 500 => "D", 100 => "C", 50 => "L", 10 => "X", 5 => "V", 1 => "I"}
+  @numeral_map %{1 => "I", 5 => "V", 10 => "X", 50 => "L", 100 => "C", 500 => "D", 1000 => "M"}
 
   def numerals(number) do
-    do_numerals(number, @numeral_map |> Map.to_list |> Enum.reverse, "")
+    @numeral_map
+    |> Map.to_list
+    |> Enum.reverse
+    |> Enum.scan(fn({num, numeral}, {acc_num, acc_numeral}) ->
+      get_numeral(number, {num, numeral}, {acc_num, acc_numeral})
+    end)
   end
 
-  def do_numerals(number, [], acc), do: acc
-  def do_numerals(number, [{head, numeral}|tail], acc) do
-    # IO.inspect head
-    # IO.inspect tail
-    # IO.puts "number: #{number}"
-    # IO.inspect numeral
-    {acc, number} =
-      case div(number, head) do
-        0 when number + number * 0.25 >= head -> {String.replace_suffix(acc, "", numeral),
-        number - head}
-        _ when div(number, head) < 0 -> {String.replace_prefix(acc, "", numeral), number + head}
-        _ ->
-        {String.replace_suffix(acc, "", String.duplicate(numeral, div(number, head))),
-        number - head * div(number, head)}
-      end
-      # IO.inspect {acc, number}
-      do_numerals(number, tail, acc)
-    end
+  def get_numeral(number, {num, numeral}, _) when number / num <= 1 and number / num >= 3 do
+    String.duplicate(numeral, number / num)
   end
+  def get_numeral(number, {num, numeral}, {acc_num, acc_numeral}) when number / num < 3 do
+
+  end
+  def get_numeral(number, _, _) do
+    number
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  # def numerals(number) do
+  #   do_numerals(number, @numeral_map |> Map.to_list |> Enum.reverse, "")
+  # end
+  #
+  # def do_numerals(_, [], acc), do: acc
+  # def do_numerals(number, [{head, numeral}|tail], acc) do
+  #   IO.puts "head: #{head}"
+  #   IO.inspect tail
+  #   IO.puts "number: #{number}"
+  #   IO.puts "numeral: #{numeral}"
+  #   {acc, number} =
+  #     cond div(number, head) do
+  #       div(number, head) <= 0 when number + number * 0.25 >= head -> {String.replace_suffix(acc, "", numeral),
+  #       number - head}
+  #       _ when div(number, head) < 0 -> {String.replace_prefix(acc, "", numeral), number + head}
+  #       _ ->
+  #       {String.replace_suffix(acc, "", String.duplicate(numeral, div(number, head))),
+  #       number - head * div(number, head)}
+  #     end
+  #     IO.inspect {acc, number}
+  #     do_numerals(number, tail, acc)
+  #   end
+end
--- elixir/roman-numerals/roman_numerals_test.exs
+++ elixir/roman-numerals/roman_numerals_test.exs
@@ -12,30 +12,37 @@ defmodule RomanTest do
     assert Roman.numerals(1) == "I"
   end
 
+  @tag :pending
   test "2" do
     assert Roman.numerals(2) == "II"
   end
 
+  @tag :pending
   test "3" do
     assert Roman.numerals(3) == "III"
   end
 
+  @tag :pending
   test "4" do
     assert Roman.numerals(4) == "IV"
   end
 
+  @tag :pending
   test "5" do
     assert Roman.numerals(5) == "V"
   end
 
+  @tag :pending
   test "6" do
     assert Roman.numerals(6) == "VI"
   end
 
+  @tag :pending
   test "9" do
     assert Roman.numerals(9) == "IX"
   end
 
+  @tag :pending
   test "27" do
     assert Roman.numerals(27) == "XXVII"
   end
@@ -60,6 +67,7 @@ defmodule RomanTest do
     assert Roman.numerals(141) == "CXLI"
   end
 
+  @tag :pending
   test "163" do
     assert Roman.numerals(163) == "CLXIII"
   end
@@ -69,6 +77,7 @@ defmodule RomanTest do
     assert Roman.numerals(402) == "CDII"
   end
 
+  @tag :pending
   test "575" do
     assert Roman.numerals(575) == "DLXXV"
   end
@@ -83,6 +92,7 @@ defmodule RomanTest do
     assert Roman.numerals(1024) == "MXXIV"
   end
 
+  @tag :pending
   test "3000" do
     assert Roman.numerals(3000) == "MMM"
   end
